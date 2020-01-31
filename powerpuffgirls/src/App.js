06780fa1f2ffc334c51b6e46929f0efa3e949790
nirupamaa jaya <nirustanite@gmail.com>
2020-01-31 14:03:47 +0100

    add error page

--- powerpuffgirls/src/App.js
+++ powerpuffgirls/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom'; //A <Switch> renders the first child <Route> that matches
 import HomePage from './components/HomePage';
 import EpisodeDetailPage from './components/EpisodeDetailPage';
 import ShowDetailPage from './components/ShowDetailPage';
+import Component404 from './components/Component404';
 import './styles/App.css';
 
 // Routing for three pages
 function App() {
   return (
       <div className='App-header'> 
-       <Route path="/" exact component={HomePage} />
-       <Route path="/show/:id" component={ShowDetailPage}/>
-       <Route path="/episode/:id" component={EpisodeDetailPage} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/show/:id' component={ShowDetailPage}/>
+        <Route exact path='/episode/:id' component={EpisodeDetailPage} />
+        <Route component={Component404} />
+       </Switch>
       </div>
   );
 }
---
+++ powerpuffgirls/src/components/Component404.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Component404 extends Component{
+
+    render(){
+        return(
+            <React.Fragment>
+                <h2> OOPS !!!</h2>
+                <h3>Page Not Found</h3>
+            </React.Fragment>
+        )
+    }
+}
